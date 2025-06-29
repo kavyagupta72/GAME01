8c1163148c76f5ca1459ff8db184bf530ac5b142
unknown <kavyaaaaaa72@gmail.com>
2025-06-29 22:06:50 +0530

    updated game structure

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let bikeuser = false;
 console.log("hiiiii");
 let all = document.querySelector(".whole");
 let choice = document.querySelectorAll(".choice");
+let button = document.querySelector(".btn");
 choice.forEach((ch) => {
     ch.addEventListener("click",()=>{
         console.log("a choice was made.");
@@ -17,5 +18,7 @@ choice.forEach((ch) => {
         window.location.href = "file:///C:/Users/HP/OneDrive/Documents/Javascript/Game01/index2.html"
     });
 })
-
+button.addEventListener("click",()=>{
+    window.location.href = "file:///C:/Users/HP/OneDrive/Documents/Javascript/Game01/index.html"
+});
 
